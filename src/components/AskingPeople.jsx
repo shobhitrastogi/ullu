fac05b62dc4de00f12f5e71f335dd2406b80c01c
shobhitrastogi <rshobhit255@gmail.com>
2024-04-01 09:11:32 +0530

    change in the code

--- src/components/AskingPeople.jsx
+++ src/components/AskingPeople.jsx
@@ -26,7 +26,7 @@ const AskingPeople = () => {
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Where Do You Live </label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <input type="text" class="form-control" id="exampleInputPassword1"/>
   </div>
   
   <button type="submit" class="btn btn-dark" onClick={handleclick}>Order-Now</button>
